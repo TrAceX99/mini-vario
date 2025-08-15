6e67dc351cb944923dbe2409b9930a4bd3a8ebe2
TrAceX <djole1271952@gmail.com>
2025-08-15 22:27:36 +0200

    Sensor test code with serial output

---
+++ main/main.c
@@ -0,0 +1,203 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
+#include "driver/i2c_master.h"
+
+#include "bmp3.h"
+
+#define ITERATION UINT8_C(100)
+#define I2C_ADDRESS BMP3_ADDR_I2C_SEC
+#define USEIIC 1
+
+i2c_master_bus_handle_t i2c_bus;
+i2c_master_dev_handle_t i2c_dev;
+
+/*!
+ * Delay function
+ */
+void bmp3_user_delay_us(uint32_t period, void *intf_ptr)
+{
+    /* Wait for a period amount of microseconds. */
+    vTaskDelay(pdMS_TO_TICKS(period / 1000));
+}
+/*!
+ * I2C read function
+ */
+BMP3_INTF_RET_TYPE bmp3_user_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr)
+{
+    esp_err_t ret;
+
+    ret = i2c_master_transmit_receive(i2c_dev, &reg_addr, 1, reg_data, len, 100);
+    if (ret != ESP_OK)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+/*!
+ * I2C write function
+ */
+BMP3_INTF_RET_TYPE bmp3_user_i2c_write(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr)
+{
+    esp_err_t ret;
+    i2c_master_transmit_multi_buffer_info_t buffer_info[2] = {
+        {
+            .write_buffer = &reg_addr,
+            .buffer_size = 1,
+        },
+        {
+            .write_buffer = reg_data,
+            .buffer_size = len,
+        },
+    };
+
+    ret = i2c_master_multi_buffer_transmit(i2c_dev, buffer_info, 2, 100);
+    if (ret != ESP_OK)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+void bmp3_check_rslt(const char api_name[], int8_t rslt)
+{
+    static const char *TAG = "BMP3";
+
+    switch (rslt)
+    {
+    case BMP3_OK:
+        /* Do nothing */
+        break;
+    case BMP3_E_NULL_PTR:
+        ESP_LOGE(TAG, "API: %s Error: Null pointer; %d", api_name, rslt);
+        break;
+    case BMP3_E_COMM_FAIL:
+        ESP_LOGE(TAG, "API: %s Error: Communication failure; %d", api_name, rslt);
+        break;
+    case BMP3_E_INVALID_LEN:
+        ESP_LOGE(TAG, "API: %s Error: Incorrect length parameter; %d", api_name, rslt);
+        break;
+    case BMP3_E_DEV_NOT_FOUND:
+        ESP_LOGE(TAG, "API: %s Error: Device not found; %d", api_name, rslt);
+        break;
+    case BMP3_E_CONFIGURATION_ERR:
+        ESP_LOGE(TAG, "API: %s Error: Configuration Error; %d", api_name, rslt);
+        break;
+    case BMP3_W_SENSOR_NOT_ENABLED:
+        ESP_LOGW(TAG, "API: %s Warning: Sensor not enabled; %d", api_name, rslt);
+        break;
+    case BMP3_W_INVALID_FIFO_REQ_FRAME_CNT:
+        ESP_LOGW(TAG, "API: %s Warning: Fifo watermark level is not in limit; %d", api_name, rslt);
+        break;
+    default:
+        ESP_LOGE(TAG, "API: %s Error: Unknown error code; %d", api_name, rslt);
+        break;
+    }
+}
+
+BMP3_INTF_RET_TYPE bmp3_interface_init(struct bmp3_dev *bmp3)
+{
+    int8_t rslt = BMP3_OK;
+
+    if (bmp3 != NULL)
+    {
+        bmp3->intf = BMP3_I2C_INTF;
+        bmp3->read = bmp3_user_i2c_read;
+        bmp3->write = bmp3_user_i2c_write;
+        bmp3->delay_us = bmp3_user_delay_us;
+        bmp3->intf_ptr = &i2c_dev;
+    }
+    else
+    {
+        rslt = BMP3_E_NULL_PTR;
+    }
+
+    return rslt;
+}
+
+int8_t rslt;
+uint16_t settings_sel;
+struct bmp3_dev dev;
+struct bmp3_data data = {0};
+struct bmp3_settings settings = {0};
+struct bmp3_status status = {{0}};
+
+void app_main(void)
+{
+    /* Interface reference is given as a parameter
+     *         For I2C : BMP3_I2C_INTF
+     *         For SPI : BMP3_SPI_INTF
+     */
+
+    i2c_master_bus_config_t bus_config = {
+        .i2c_port = I2C_NUM_0,
+        .sda_io_num = 6,
+        .scl_io_num = 7,
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .glitch_ignore_cnt = 7,
+        .flags.enable_internal_pullup = true,
+    };
+    ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &i2c_bus));
+
+    i2c_device_config_t dev_config = {
+        .dev_addr_length = I2C_ADDR_BIT_LEN_7,
+        .device_address = BMP3_ADDR_I2C_SEC,
+        .scl_speed_hz = 400000,
+    };
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(i2c_bus, &dev_config, &i2c_dev));
+
+    bmp3_interface_init(&dev);
+    rslt = bmp3_init(&dev);
+    bmp3_check_rslt("bmp3_init", rslt);
+
+    settings.int_settings.drdy_en = BMP3_ENABLE;
+    settings.press_en = BMP3_ENABLE;
+    settings.temp_en = BMP3_ENABLE;
+
+    settings.odr_filter.press_os = BMP3_OVERSAMPLING_8X;
+    settings.odr_filter.temp_os = BMP3_NO_OVERSAMPLING;
+    settings.odr_filter.odr = BMP3_ODR_50_HZ;
+    settings.odr_filter.iir_filter = BMP3_IIR_FILTER_COEFF_7;
+
+    settings_sel = BMP3_SEL_PRESS_EN | BMP3_SEL_TEMP_EN | BMP3_SEL_PRESS_OS | BMP3_SEL_TEMP_OS | BMP3_SEL_ODR | BMP3_SEL_IIR_FILTER | BMP3_SEL_DRDY_EN;
+
+    rslt = bmp3_set_sensor_settings(settings_sel, &settings, &dev);
+    bmp3_check_rslt("bmp3_set_sensor_settings", rslt);
+
+    settings.op_mode = BMP3_MODE_NORMAL;
+    rslt = bmp3_set_op_mode(&settings, &dev);
+    bmp3_check_rslt("bmp3_set_op_mode", rslt);
+
+    while (1)
+    {
+        rslt = bmp3_get_status(&status, &dev);
+        bmp3_check_rslt("bmp3_get_status", rslt);
+
+        /* Read temperature and pressure data iteratively based on data ready interrupt */
+        if ((rslt == BMP3_OK) && (status.intr.drdy == BMP3_ENABLE))
+        {
+            rslt = bmp3_get_sensor_data(BMP3_PRESS_TEMP, &data, &dev);
+            bmp3_check_rslt("bmp3_get_sensor_data", rslt);
+
+            /* NOTE : Read status register again to clear data ready interrupt status */
+            rslt = bmp3_get_status(&status, &dev);
+            bmp3_check_rslt("bmp3_get_status", rslt);
+
+            printf("T: %.2f deg C, P: %.2f Pa\n", data.temperature, data.pressure);
+        }
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
